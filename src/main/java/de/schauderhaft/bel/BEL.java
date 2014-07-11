be36966aa416154c79c0ef37f4938046649f2ce2
Arno Haase <arno.haase@haase-consulting.com>
2014-07-11 22:03:37 +0200

    cleanup

--- src/main/java/de/schauderhaft/bel/BEL.java
+++ src/main/java/de/schauderhaft/bel/BEL.java
@@ -1,54 +1,9 @@
 package de.schauderhaft.bel;
 
-<<<<<<< HEAD
-import de.schauderhaft.bel.friends.Friend;
-
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
-=======
->>>>>>> cleanup
 
 public class BEL{
 
     public static void main(String args[]){
         new BEL();
     }
-<<<<<<< HEAD
-
-    public BEL(){
-        // read ip adresses+ port to connect to
-        List<Friend> friends  = readFriends();
-
-        for (Friend friend : friends) {
-            send(friend, "hello world");
-        }
-
-    }
-
-    private void send(Friend friend, String s) {
-    }
-
-    private List<Friend> readFriends() {
-        ArrayList<Friend> friends = new ArrayList<Friend>();
-        try {
-            FileInputStream fis = new FileInputStream("friends.txt");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
-
-            String line = reader.readLine();
-            while(line != null){
-                System.out.println(line);
-                String[] parts = line.split(":");
-                friends.add(new Friend(parts[0],parts[1], parts[2]));
-                line = reader.readLine();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return friends;
-    }
-=======
->>>>>>> cleanup
 }
