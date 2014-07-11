075b5fef7ef479671fcce7616bb2aa17a3feb6ec
Thomas Michael <thomas.michael@anmi24.de>
2014-07-11 21:49:25 +0200

    added ugly gui

---
+++ src/main/java/de/schauderhaft/bel/client/view/BelViewController.java
@@ -0,0 +1,42 @@
+package de.schauderhaft.bel.client.view;
+
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class BelViewController {
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private TextArea chatbox;
+
+	@FXML
+	private TextField chatinput;
+
+	@FXML
+	private Button pushButton;
+
+	@FXML
+	void pushInput(ActionEvent event) {
+	}
+
+	@FXML
+	void pushinput(KeyEvent event) {
+	}
+
+	@FXML
+	void initialize() {
+		assert chatbox != null : "fx:id=\"chatbox\" was not injected: check your FXML file 'ChatClient.fxml'.";
+		assert chatinput != null : "fx:id=\"chatinput\" was not injected: check your FXML file 'ChatClient.fxml'.";
+		assert pushButton != null : "fx:id=\"pushButton\" was not injected: check your FXML file 'ChatClient.fxml'.";
+
+	}
+
+}
---
+++ src/main/java/de/schauderhaft/bel/client/view/ChatClientStarter.java
@@ -0,0 +1,27 @@
+package de.schauderhaft.bel.client.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChatClientStarter extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		Parent root = FXMLLoader
+				.load(getClass().getResource("ChatClient.fxml"));
+		Scene scene = new Scene(root);
+
+		stage.setTitle("Chat Client");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
