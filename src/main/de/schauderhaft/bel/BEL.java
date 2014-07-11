73e602fe786974f2698d5a77542453bbed2fe5c5
Jens Schauder <jens.schauder@freenet.de>
2014-07-11 20:33:15 +0200

    first structure

--- src/main/de/schauderhaft/bel/BEL.java
+++ src/main/de/schauderhaft/bel/BEL.java
@@ -2,6 +2,10 @@ package de.schauderhaft.bel;
 
 public class BEL{
     public static void main(String args[]){
+            // read ip adresses+ port to connect to
 
+            // send a hello message to all
+
+            
     }
 }
