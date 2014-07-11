c863446eda2a312b2a099ea476cdf39d28dedbe2
Jens Schauder <jens.schauder@freenet.de>
2014-07-11 21:18:45 +0200

    Friend has attributes

--- src/main/java/de/schauderhaft/bel/message/Friend.java
+++ src/main/java/de/schauderhaft/bel/message/Friend.java
@@ -1,5 +1,14 @@
 package de.schauderhaft.bel.message;
 
 public class Friend {
+    public final String name;
+    public final String ip;
+    public final String port;
 
+    public Friend(String name, String ip, String port) {
+
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+    }
 }
